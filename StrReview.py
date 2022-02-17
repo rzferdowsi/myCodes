a5c4d91e9c58b478b16c62f58b79e8e0e8494b1d
Reza Ferdowsi <97004718+rzferdowsi@users.noreply.github.com>
2022-02-16 23:29:54 -0500

    every_other_letter

--- StrReview.py
+++ StrReview.py
@@ -113,3 +113,9 @@ print(every_other_letter("Hello world!"))
 # should print Hlowrd
 print(every_other_letter(""))
 # should print 
+"""secand way"""
+def every_other_letter(word):
+  every_other = ""
+  for i in range(0, len(word), 2):
+    every_other += word[i]
+  return every_other
