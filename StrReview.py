9a6c7aca2ea87ced3471cc20c3963500e1b3fbc3
Reza Ferdowsi <97004718+rzferdowsi@users.noreply.github.com>
2022-02-16 23:24:33 -0500

    every_other_letter
    returns even letters in text.

--- StrReview.py
+++ StrReview.py
@@ -96,3 +96,20 @@ print(check_for_name("My name is jamie", "Jamie"))
 # should print True
 print(check_for_name("My name is Samantha", "Jamie"))
 # should print False
+
+"""Create a function named every_other_letter that takes a string named word as a parameter. The function should return a string containing every other letter in word."""
+
+# Write your every_other_letter function here:
+def every_other_letter(word):
+   y = ""
+   for i in range(len(word)):
+     if i % 2 ==0:
+       y +=word[i]
+   return y
+# Uncomment these function calls to test your function:
+print(every_other_letter("Codecademy"))
+# should print Cdcdm
+print(every_other_letter("Hello world!"))
+# should print Hlowrd
+print(every_other_letter(""))
+# should print 
