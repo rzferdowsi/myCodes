f167ff4c5457aa1987c49d12957115f1fe323a80
Reza Ferdowsi <97004718+rzferdowsi@users.noreply.github.com>
2022-02-17 18:45:59 -0500

    sum_even_keys

--- DicReview.py
+++ DicReview.py
@@ -12,3 +12,19 @@ print(sum_values({"milk":5, "eggs":2, "flour": 3}))
 # should print 10
 print(sum_values({10:1, 100:2, 1000:3}))
 # should print 6
+
+"""Create a function called sum_even_keys that takes a dictionary named my_dictionary, with all integer keys and values, as a parameter. This function should return the sum of the values of all even keys."""
+
+# Write your sum_even_keys function here:
+def sum_even_keys(my_dictionary):
+  s = 0
+  for key in my_dictionary:
+    if key % 2 == 0:
+      s+= my_dictionary[key]
+  return s
+   
+# Uncomment these function calls to test your  function:
+print(sum_even_keys({1:5, 2:2, 3:3}))
+# should print 2
+print(sum_even_keys({10:1, 100:2, 1000:3}))
+# should print 6
