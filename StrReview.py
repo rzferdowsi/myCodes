5af127a92fd748802bb678bcc333fe500a212a1a
Reza Ferdowsi <97004718+rzferdowsi@users.noreply.github.com>
2022-02-17 14:52:29 -0500

    Update StrReview.py

--- StrReview.py
+++ StrReview.py
@@ -152,3 +152,16 @@ print(make_spoonerism("Hello", "world!"))
 print(make_spoonerism("a", "b"))
 # should print b a
 
+# Write your add_exclamation function here:
+def add_exclamation(word):
+  x =len(word)
+  if x >= 20:
+    return word
+  else:
+    return word+ (20-x)*"!"
+"""Create a function named add_exclamation that has one parameter named word. This function should add exclamation points to the end of word until word is 20 characters long. If word is already at least 20 characters long, just return word."""
+# Uncomment these function calls to test your function:
+print(add_exclamation("Codecademy"))
+# should print Codecademy!!!!!!!!!!
+print(add_exclamation("Codecademy is the best place to learn"))
+# should print Codecademy is the best place to learn
