5540cdc0e25f2775dd45f8c858ee19b1fa5893c6
Reza Ferdowsi <97004718+rzferdowsi@users.noreply.github.com>
2022-02-16 23:00:30 -0500

    adding check_for_name function
    this function checks if substring exist in string.

--- StrReview.py
+++ StrReview.py
@@ -77,3 +77,22 @@ print(substring_between_letters("apple", "p", "e"))
 print(substring_between_letters("apple", "l", "e"))
 # should print "apple"
 print("__________")
+
+"""Write a function called check_for_name that takes two strings as parameters named sentence and name. The function should return True if name appears in sentence in all lowercase letters, all uppercase letters, or with any mix of uppercase and lowercase letters. The function should return False otherwise.
+
+For example, the following three calls should all return True:"""
+
+# Write your check_for_name function here:
+def check_for_name(sentence, name):
+  s = sentence.lower()
+  n = name.lower()
+  if n in s:
+    return True
+  return False
+# Uncomment these function calls to test your  function:
+print(check_for_name("My name is Jamie", "Jamie"))
+# should print True
+print(check_for_name("My name is jamie", "Jamie"))
+# should print True
+print(check_for_name("My name is Samantha", "Jamie"))
+# should print False
