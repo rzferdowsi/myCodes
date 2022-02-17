ecb9fc6a71af9297cb18dfd9c627105777860de4
Reza Ferdowsi <97004718+rzferdowsi@users.noreply.github.com>
2022-02-16 21:59:05 -0500

    Update StrReview.py

--- StrReview.py
+++ StrReview.py
@@ -46,3 +46,17 @@ print(count_multi_char_x("apple", "pp"))
 """Write a function named substring_between_letters that takes a string named word, a single character named start, and another character named end. This function should return the substring between the first occurrence of start and end in word. If start or end are not in word, the function should return word.
 
 For example, substring_between_letters("apple", "p", "e") should return "pl"."""
+# Write your substring_between_letters function here:
+def substring_between_letters(word, start,end):
+  if start in word and end in word:
+    s = word.find(start)
+    e = word.find(end)
+    return word[s+1:e]
+  else:
+    return word
+
+# Uncomment these function calls to test your function:
+print(substring_between_letters("apple", "p", "e"))
+# should print "pl"
+print(substring_between_letters("apple", "l", "e"))
+# should print "apple"
