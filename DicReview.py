46bba942269a2a7cf48aa4a3c53909e82a1151ee
Reza Ferdowsi <97004718+rzferdowsi@users.noreply.github.com>
2022-02-17 19:00:53 -0500

    add_ten

--- DicReview.py
+++ DicReview.py
@@ -28,3 +28,15 @@ print(sum_even_keys({1:5, 2:2, 3:3}))
 # should print 2
 print(sum_even_keys({10:1, 100:2, 1000:3}))
 # should print 6
+
+"""Create a function named add_ten that takes a dictionary with integer values named my_dictionary as a parameter. The function should add 10 to every value in my_dictionary and return my_dictionary"""
+# Write your add_ten function here:
+def add_ten(my_dictionary):
+  for key in my_dictionary:
+    my_dictionary[key] +=10
+  return  my_dictionary 
+# Uncomment these function calls to test your  function:
+print(add_ten({1:5, 2:2, 3:3}))
+# should print {1:15, 2:12, 3:13}
+print(add_ten({10:1, 100:2, 1000:3}))
+# should print {10:11, 100:12, 1000:13}
