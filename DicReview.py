3b34133ce4f3525d95e2a9296cad21f85891379a
Reza Ferdowsi <97004718+rzferdowsi@users.noreply.github.com>
2022-02-17 19:32:56 -0500

    max_key

--- DicReview.py
+++ DicReview.py
@@ -55,3 +55,18 @@ print(values_that_are_keys({1:100, 2:1, 3:4, 4:10}))
 # should print [1, 4]
 print(values_that_are_keys({"a":"apple", "b":"a", "c":100}))
 # should print ["a"]
+
+"""Write a function named max_key that takes a dictionary named my_dictionary as a parameter. The function should return the key associated with the largest value in the dictionary."""
+
+# Write your max_key function here:
+def max_key(my_dictionary): 
+  vl = list(my_dictionary.values())
+  kl = list(my_dictionary.keys())
+  maxv = max(vl)
+  i = vl.index(maxv)
+  return kl[i]
+# Uncomment these function calls to test your  function:
+print(max_key({1:100, 2:1, 3:4, 4:10}))
+# should print 1
+print(max_key({"a":100, "b":10, "c":1000}))
+# should print "c"
