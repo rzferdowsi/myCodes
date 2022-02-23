382d1e8f2ed18d22e287c3f2994a2874740c9a23
Reza Ferdowsi <97004718+rzferdowsi@users.noreply.github.com>
2022-02-22 19:23:01 -0500

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -79,3 +79,38 @@ clinic_north_south = df[['clinic_north','clinic_south']]
 
 print(clinic_north_south,"\n",type(clinic_north_south))
 
+# Select Rows
+import codecademylib3
+import pandas as pd
+
+df = pd.DataFrame([
+  ['January', 100, 100, 23, 100],
+  ['February', 51, 45, 145, 45],
+  ['March', 81, 96, 65, 96],
+  ['April', 80, 80, 54, 180],
+  ['May', 51, 54, 54, 154],
+  ['June', 112, 109, 79, 129]],
+  columns=['month', 'clinic_east',
+           'clinic_north', 'clinic_south',
+           'clinic_west'])
+march = df.iloc[1:4:2]
+print(march, type(march))
+
+#  Selecting Multiple Rows
+
+import codecademylib3
+import pandas as pd
+
+df = pd.DataFrame([
+  ['January', 100, 100, 23, 100],
+  ['February', 51, 45, 145, 45],
+  ['March', 81, 96, 65, 96],
+  ['April', 80, 80, 54, 180],
+  ['May', 51, 54, 54, 154],
+  ['June', 112, 109, 79, 129]],
+  columns=['month', 'clinic_east',
+           'clinic_north', 'clinic_south',
+           'clinic_west']
+)
+april_may_june = df.iloc[3:6]
+print(april_may_june)
