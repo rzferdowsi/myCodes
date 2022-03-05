f284cc69ef57a27210f420b919950d7c4576121b
Reza Ferdowsi <97004718+rzferdowsi@users.noreply.github.com>
2022-03-04 21:23:42 -0500

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -314,3 +314,56 @@ print(inventory.head())
 combine_lambda = lambda row: '{} - {}'.format(row.product_type,row.product_description)
 inventory['full_description'] = inventory.apply(combine_lambda,axis=1)
 print(inventory.head())
+
+"""AGGREGATES IN PANDAS"""
+
+# Calculating Column Statistics
+
+import numpy as np
+import pandas as pd
+
+orders = pd.read_csv('orders.csv')
+print(orders.head(10))
+most_expensive = orders.price.max()
+num_colors = orders.shoe_color.nunique()
+
+#Calculating Aggregate Functions
+
+orders = pd.read_csv('orders.csv')
+print(orders.head())
+pricey_shoes = orders.groupby('shoe_type').price.max()
+print(pricey_shoes)
+print(type(pricey_shoes))
+
+orders = pd.read_csv('orders.csv')
+
+pricey_shoes = (orders.groupby('shoe_type').price.max().reset_index()).rename(columns={'price':'highest_price'})
+print(pricey_shoes)
+print(type(pricey_shoes))
+
+orders = pd.read_csv('orders.csv')
+cheap_shoes = orders.groupby('shoe_color').price.apply(lambda x: np.percentile(x, 25)).reset_index()
+print(cheap_shoes)
+orders = pd.read_csv('orders.csv')
+print(orders)
+shoe_counts = orders.groupby(['shoe_type','shoe_color']).id.count().reset_index()
+print(shoe_counts)
+
+orders = pd.read_csv('orders.csv')
+
+shoe_counts = orders.groupby(['shoe_type', 'shoe_color']).id.count().reset_index()
+print(shoe_counts)
+shoe_counts_pivot = shoe_counts.pivot(columns='shoe_color', index='shoe_type',values='id').reset_index()
+print(shoe_counts_pivot)
+
+import pandas as pd
+
+user_visits = pd.read_csv('page_visits.csv')
+print(user_visits.head())
+click_source = user_visits.groupby('utm_source').id.count().reset_index()
+print(click_source)
+
+click_source_by_month = user_visits.groupby(['utm_source',"month"]).id.count().reset_index()
+print(click_source_by_month)
+click_source_by_month_pivot = click_source_by_month.pivot(columns="month",index= 'utm_source', values = 'id').reset_index()
+print(click_source_by_month_pivot)
